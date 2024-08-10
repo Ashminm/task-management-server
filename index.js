d3e2642e70eddee546077308ffd86ceee7eb797a
Ashmin <ashmin2802@gmail.com>
2024-08-10 11:58:52 +0530

    index chang and finish all

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const express =require('express')
 const cors=require('cors')
-
 const tasks=express()
+
+require('./Connection/database')
 // cors use
 tasks.use(cors())
 // express use
 tasks.use(express.json())
 
 const router=require('./Routers/route')
-require('./Connection/database')
 
 // router use
 tasks.use(router)
